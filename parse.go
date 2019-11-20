27973d2f6580c6b3c3d1796396c04c5b35180f86
clairemuller <clairermuller@gmail.com>
2019-11-19 17:31:06 -0800

    refactor to find only link nodes

--- parse.go
+++ parse.go
@@ -7,6 +7,10 @@ import (
 	"golang.org/x/net/html"
 )
 
+// 1. find the <a> nodes
+// 2. create a Link instance for each <a> node
+// 3. return all the Links in a slice
+
 // Link represents a link <a href="..."> in an HTML document
 type Link struct {
 	Href string
@@ -16,23 +20,28 @@ type Link struct {
 // Parse takes an HTML document and returns a slice of links parsed from it
 func Parse(r io.Reader) ([]Link, error) {
 	// html.Parse returns the root of the parse tree as a *Node from the given Reader
-	doc, err := html.Parse(r)
+	rootNode, err := html.Parse(r)
 	if err != nil {
 		return nil, err
 	}
 
-	dfs(doc, "")
-	// 1. find the <a> nodes
-	// 2. create a Link instance for each <a> node
-	// 3. return all the Links in a slice
+	nodes := linkNodes(rootNode)
+
+	for _, node := range nodes {
+		fmt.Println(node)
+	}
 
 	return nil, nil
 }
 
-func dfs(n *html.Node, padding string) {
-	// n.Data will give us what's inside the node
-	fmt.Println(padding, n.Data)
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		dfs(c, padding+"  ")
+// takes in root node, returns all link nodes
+func linkNodes(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
 	}
+	var nodeSlice []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		nodeSlice = append(nodeSlice, linkNodes(c)...)
+	}
+	return nodeSlice
 }
